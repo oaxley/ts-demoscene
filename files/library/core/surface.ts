67c2fcc9ba464b780a6c06a88a00f2bdc0e82e31
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-02 19:48:00 -0400

    Surface generic class

---
+++ files/library/core/surface.ts
@@ -0,0 +1,66 @@
+/*
+ * @file    surface.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   A drawing surface
+ */
+
+//----- imports
+import { Size, Rect, Point2D } from "./interfaces";
+
+
+//----- class
+export class Surface {
+    //----- members
+    private width_: number;
+    private height_: number;
+
+    private canvas_: HTMLCanvasElement;
+    private context_: CanvasRenderingContext2D;
+
+
+    //----- methods
+    constructor(size: Size) {
+
+        // create a new canvas element
+        this.canvas_ = document.createElement("canvas")
+        this.canvas_.width  = size.width;
+        this.canvas_.height = size.height;
+
+        // setup vars
+        this.width_   = size.width;
+        this.height_  = size.height;
+        this.context_ = this.canvas_.getContext("2d");
+    }
+
+    public get context(): CanvasRenderingContext2D {
+        return this.context_;
+    }
+
+    public get canvas(): HTMLCanvasElement {
+        return this.canvas_;
+    }
+
+    public get size(): Size {
+        return {
+            width: this.width_,
+            height: this.height_
+        }
+    }
+
+    public get width(): number {
+        return this.width;
+    }
+
+    public get height(): number {
+        return this.height;
+    }
+
+    public get data(): ImageData {
+        return this.context_.getImageData(0, 0, this.width_, this.height_);
+    }
+
+    public set data(image: ImageData) {
+        this.context_.putImageData(image, 0, 0);
+    }
+}
