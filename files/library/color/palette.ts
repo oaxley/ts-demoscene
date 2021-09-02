197eb73e29dc775d55c8687e062a8f4d80184528
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-01 22:22:45 -0400

    Function to create a gradient of colors

--- files/library/color/palette.ts
+++ files/library/color/palette.ts
@@ -7,6 +7,7 @@
 
 //----- imports
 import { Color } from "./color";
+import { Components } from "./basecolor";
 
 //----- class
 export class Palette {
@@ -64,4 +65,42 @@ export class Palette {
         [ this.colors_[i1], this.colors_[i2] ] = [ this.colors_[i2], this.colors_[i1] ];
     }
 
+    public gradient(begin: Color, end: Color, steps: number): void {
+        // ensure both color are in the same space
+        if ( begin.color.model != end.color.model ) {
+            throw Error("gradient(): Both colors need to be in the same space.");
+        }
+
+        let x: number, y: number, z: number, a: number;
+        let dx: number, dy: number, dz: number, da: number;
+
+        let v1: Components = begin.color.values;
+        let v2: Components = end.color.values;
+
+        // ensure both colors are from the same color space
+        if ( v1.m != v2.m ) {
+            throw Error("Both colors should be from the same color space.");
+        }
+
+        // initialize values
+        [ x, y, z, a ] = [ v1.x, v1.y, v1.z, v1.a ];
+
+        // (-1) so we are sure to go from c1 up to c2
+        dx = (v2.x - x) / (steps - 1);
+        dy = (v2.y - y) / (steps - 1);
+        dz = (v2.z - z) / (steps - 1);
+        da = (v2.a - a) / (steps - 1);
+
+        // create colors
+        for(var i = 0; i < steps; i++) {
+            // add this new color
+            this.colors_.push( new Color(v1.m, x, y, z, a) );
+
+            // increment values
+            x += dx;
+            y += dy;
+            z += dz;
+            a += da;
+        }
+    }
 }
