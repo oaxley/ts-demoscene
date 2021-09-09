0391ecdfd107a08a069a56beb9fa07cdcfdb8ec4
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-09 18:57:04 -0400

    map & constrain functions

--- files/library/maths/utils.ts
+++ files/library/maths/utils.ts
@@ -31,3 +31,42 @@ export function degrees(angle: number): number {
     return angle * 180 / Math.PI;
 }
 
+/* map a value from a value domain to another one
+ *
+ * Args
+ *  value   : the value to map
+ *  src_low : the lowest boundary for 'value' in its current domain
+ *  src_high: the highest boundary for 'value' in its current domain
+ *  dst_low : the destination lowest boundary
+ *  dst_high: the destination highest boundary
+ *
+ * Returns
+ *  the new value within the destination domain
+ */
+export function map(value: number, src_low: number, src_high: number, dst_low: number, dst_high: number): number {
+    let src_range = src_high - src_low;
+    let dst_range = dst_high - dst_low;
+
+    return dst_low + (1.0 * dst_range * (value - src_low)) / src_range;
+}
+
+/* constrain a value between a range
+ *
+ * Args
+ *  value: the value to constrain
+ *  min  : the lowest acceptable boundary
+ *  max  : the highest acceptable boundaary
+ *
+ * Returns
+ *  the new value within the boundaries
+ */
+export function constrain(value: number, min: number, max: number): number {
+    if (value < min)
+        return min
+    else {
+        if (value > max)
+            return max
+        else
+            return value
+    }
+}
