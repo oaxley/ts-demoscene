121c5b38f3a4788c4f975921499f4d282befbf07
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-13 19:49:14 -0400

    Add comments + image loader on display back surface

--- files/library/core/display.ts
+++ files/library/core/display.ts
@@ -24,27 +24,38 @@ export class Display {
         this.surface_ = new Surface({width: canvas.width, height: canvas.height});
     }
 
+    // accessor to get the display width
     public get width(): number {
         return this.screen_.width;
     }
 
+    // accessor to get the display height
     public get height(): number {
         return this.screen_.height;
     }
 
+    // draw the display back surface on the canvas element
     public draw() {
         this.context_.drawImage(this.surface_.canvas, 0, 0);
     }
 
+    // retrieve the display back surface
     public get surface(): Surface {
         return this.surface_;
     }
 
+    // retrieve the screen canvas element
     public get screen(): HTMLCanvasElement {
         return this.screen_;
     }
 
+    // clear the screen
     public clear() {
         this.context_.clearRect(0, 0, this.screen_.width, this.screen_.height);
+    }
+
+    // load an image on the display back Surface
+    public loadImage(image: HTMLImageElement) {
+        this.surface_.context.drawImage(image, 0, 0);
     }
 }
