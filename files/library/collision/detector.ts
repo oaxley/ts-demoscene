a8a92d723c396d0e07a3a19ee54c7e61f41ff4c6
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-16 18:45:32 -0400

    Create the broader collision algorithm detector

--- files/library/collision/detector.ts
+++ files/library/collision/detector.ts
@@ -56,4 +56,46 @@ export class CollisionDetector {
         });
     }
 
+    // compute the broader collision detection (based on bounding rectangle boxes)
+    private broaderCollision(): CollisionResult[] {
+        let collisions: CollisionResult[] = [];
+
+        // got through all the objects
+        for (let i = 0; i < this.objects_.length; i++) {
+
+            // only moving objects will be checked against wall
+            if (this.objects_[i] instanceof CollisionWall)
+                continue;
+
+            // compute the bounding box of this object after simulating its next move
+            let center: Vector2D = this.objects_[i].center.add(this.objects_[i].velocity);
+            let left = this.objects_[i].rect(center);
+
+            // check this object against all the other objects in the scene
+            for (let j = 0; j < this.objects_.length; j++) {
+                // no collision detection between ourself
+                if (i == j)
+                    continue;
+
+                // find the object (right) we might collide with
+                let right: Rect = null;
+                if (this.objects_[j] instanceof CollisionWall) {
+                    // wall don't move
+                    right = this.objects_[j].rect();
+                } else {
+                    // compute the next move for this object
+                    center = this.objects_[j].center.add(this.objects_[j].velocity);
+                    right  = this.objects_[j].rect(center);
+                }
+
+                // check for rectangle collision
+                if ( (left.x < right.x + right.w) && (left.x + left.w > right.x)  &&
+                     (left.y < right.y + right.h) && (left.y + left.h > right.y) ) {
+                    collisions.push({left: i, right: j});
+                }
+            }
+        }
+
+        return collisions;
+    }
 }
