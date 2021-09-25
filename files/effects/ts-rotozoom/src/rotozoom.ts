52ad02a4295ed92d7aa1ce1d74d7c530abe29c45
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-25 15:19:04 -0400

    ts-rotozoom: create the animation effect

--- files/effects/ts-rotozoom/src/rotozoom.ts
+++ files/effects/ts-rotozoom/src/rotozoom.ts
@@ -19,6 +19,8 @@ export class Rotozoom extends Animation {
     private display_  : Display;
     private image_    : Surface;
 
+    private angle_: number;                         // current rotation angle
+
     private cos_ : number[];                        // cos lookup table
     private sin_ : number[];                        // sin lookup table
 
@@ -29,6 +31,7 @@ export class Rotozoom extends Animation {
 
         // set the vars
         this.display_ = display;
+        this.angle_ = 0;
 
         // load the texture image
         let img = new Image();
@@ -68,6 +71,57 @@ export class Rotozoom extends Animation {
     protected update(timestamp: number): void {
         if (!this.isAnimated)
             return;
+
+        // retrieve the image / backbuffer data
+        let srcdata = this.image_.data;
+        let dstdata = this.display_.surface.data;
+
+        // height of the texture
+        const height: number = this.image_.canvas.height;
+
+        // cosinus / sinus lookup
+        const cs: number = this.cos_[this.angle_];
+        const sn: number = this.sin_[this.angle_];
+
+        // offset in the destination data (display surface)
+        let dstoff: number = 0;
+
+        // go through all the pixels on the screen
+        for (let y: number = 0; y < this.display_.height; y++) {
+            for (let x: number = 0; x < this.display_.width; x++) {
+
+                /* apply the rotation to the screen pixels:
+                 * x' = x * cos - y * sin
+                 * y' = x * sin + y * cos
+                 * we multiply by (sin + 1) to have a nice zoom effect
+                 */
+                let xb: number = Math.floor((x * cs - y * sn) * (sn + 1));
+                let yb: number = Math.floor((x * sn + y * cs) * (sn + 1));
+
+                /* convert the coordinate to the texture coordinate */
+                let u: number = xb & 0xff;
+                let v: number = yb % height;
+
+                while (v < 0) {
+                    v += height;
+                }
+
+                // offset in the texture (256x256)
+                let srcoff: number = (u + (v << 8)) << 2;
+
+                // copy the rgba values to display surface
+                dstdata.data[dstoff++] = srcdata.data[srcoff++];
+                dstdata.data[dstoff++] = srcdata.data[srcoff++];
+                dstdata.data[dstoff++] = srcdata.data[srcoff++];
+                dstdata.data[dstoff++] = srcdata.data[srcoff++];
+            }
+        }
+
+        // put the pixels back
+        this.display_.surface.data = dstdata;
+
+        // increase the rotation angle
+        this.angle_ = (this.angle_ + 1) % 360;
     }
 
     // render the animation on screen
