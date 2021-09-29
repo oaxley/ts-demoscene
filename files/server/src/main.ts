e0a0c3bcaa1465e78f376a4dd9149f31892f2be4
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-28 21:19:11 -0400

    Add slider buttons control

--- files/server/src/main.ts
+++ files/server/src/main.ts
@@ -5,9 +5,13 @@
  * @brief   Typescript file for the main page
  */
 
+import { threadId } from "worker_threads";
+
 //----- imports
 
 //----- globals
+const PREVIOUS_BUTTON = 0;
+const NEXT_BUTTON = 1;
 
 
 //----- interfaces
@@ -23,6 +27,13 @@ interface Config {
     title: string
 }
 
+// button parameters
+interface Button {
+    enable: boolean,
+    button: HTMLButtonElement
+}
+
+
 //----- functions
 
 
@@ -30,22 +41,24 @@ interface Config {
 class Slider {
 
     //----- members
-    private display_: HTMLCanvasElement;
-    private config_: Config[];
+    private display_: HTMLCanvasElement;        // the display canvas
+    private buttons_: Button[];                 // element buttons
 
-    private cImage_: HTMLImageElement;          // current image displayed
+    private config_: Config[];                  // configuration from NodeJS
+
     private nImage_: HTMLImageElement;          // next image to display
 
     private maxImage_: number;                  // max number of images loaded
     private curImage_: number;                  // current image displayed
 
+
     //----- methods
-    constructor(output: HTMLCanvasElement) {
+    constructor(output: HTMLCanvasElement, prev: HTMLButtonElement, next: HTMLButtonElement) {
         // set the vars
         this.display_ = output;
-        this.config_ = [];
 
         // retrieve the configuration from the NodeJS server
+        this.config_ = [];
         this.request().then((result) => {
             this.config_ = result.json()['effects'];
 
@@ -56,6 +69,25 @@ class Slider {
             // load the first image
             this.loadImage(this.curImage_);
         });
+
+        // set the buttons properties
+        this.buttons_ = [
+            {
+                enable: false,
+                button: prev
+            },
+            {
+                enable: true,
+                button: next
+            }
+        ]
+
+        // add handlers to the buttons
+        prev.addEventListener('click', () => {this.previousImage()});
+        next.addEventListener('click', () => {this.nextImage()});
+
+        // set the previous button as inactive
+        prev.className += " inactive";
     }
 
     // parse the result from the Ajax query
@@ -91,16 +123,64 @@ class Slider {
 
         this.nImage_ = new Image();
         this.nImage_.onload = () => {
-            console.log(`loading ${name}`);
             this.display_.getContext("2d").drawImage(this.nImage_, 0, 0);
         };
         this.nImage_.src = '/images/' + name;
     }
+
+    // next image
+    public nextImage(): void {
+        if (!this.buttons_[NEXT_BUTTON].enable)
+            return
+
+        this.curImage_ = this.curImage_ + 1;
+
+        // set the next button as inactive if we reached the end
+        if (this.curImage_ == this.maxImage_ - 1) {
+            this.buttons_[NEXT_BUTTON].enable = false;
+            this.buttons_[NEXT_BUTTON].button.className += " inactive";
+        }
+
+        // load the new image
+        this.loadImage(this.curImage_);
+
+        // activate the previous button
+        if (!this.buttons_[PREVIOUS_BUTTON].enable) {
+            this.buttons_[PREVIOUS_BUTTON].enable = true;
+            this.buttons_[PREVIOUS_BUTTON].button.className = "prev";
+        }
+    }
+
+    // previous image
+    public previousImage(): void {
+        if (!this.buttons_[PREVIOUS_BUTTON].enable)
+            return
+
+        this.curImage_ = this.curImage_ - 1;
+
+        // set the previous button as inactive if we reached the beginning
+        if (this.curImage_ == 0) {
+            this.buttons_[PREVIOUS_BUTTON].enable = false;
+            this.buttons_[PREVIOUS_BUTTON].button.className += " inactive";
+        }
+
+        // load the new image
+        this.loadImage(this.curImage_);
+
+        // activate the next button
+        if (!this.buttons_[NEXT_BUTTON].enable) {
+            this.buttons_[NEXT_BUTTON].enable = true;
+            this.buttons_[NEXT_BUTTON].button.className = "next";
+        }
+    }
 }
 
 //----- begin
-// retrieve the output canvas
+// retrieve the elements from the page
 let output = <HTMLCanvasElement> document.getElementById("output");
+let prev = <HTMLButtonElement> document.getElementById("prev");
+let next = <HTMLButtonElement> document.getElementById("next");
+
 
 // create a new instance of the slider
-let slider = new Slider(output);
+let slider = new Slider(output, prev, next);
