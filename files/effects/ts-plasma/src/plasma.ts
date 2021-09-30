4d48d07f860f681b3fe28061ec2e55827f71c8fd
slegrand <ut5svsxgc@relay.firefox.com>
2021-09-30 19:13:32 -0400

    ts-plasma: basic animation framework

---
+++ files/effects/ts-plasma/src/plasma.ts
@@ -0,0 +1,63 @@
+/*
+ * @file    plasma.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Plasma effect
+ */
+
+//----- imports
+import { Animation } from "library/core/animation";
+import { Display } from "library/core/display";
+
+
+//----- class
+export class Plasma extends Animation {
+
+    //----- members
+    private display_ : Display;
+
+
+    //----- methods
+    constructor(display: Display) {
+        super();
+
+        // set the vars
+        this.display_ = display;
+    }
+
+    // run the animation
+    public run(): void {
+        console.log("Starting the Lens animation.");
+
+        // toggle the animation
+        this.toggle();
+
+        // run the animation on the next frame
+        requestAnimationFrame(this.main.bind(this));
+    }
+
+    // update the animation
+    protected update(timestamp: number): void {
+        if (!this.isAnimated)
+            return;
+    }
+
+    // render the animation on screen
+    protected render(timestamp: number): void {
+        if (!this.isAnimated)
+            return;
+
+        // flip the back-buffer onto the screen
+        this.display_.draw();
+
+        // increase frames count
+        this.frames_++;
+    }
+
+    // main animation function
+    protected main(timestamp: number): void {
+        this.update(timestamp);
+        this.render(timestamp);
+        requestAnimationFrame(this.main.bind(this));
+    }
+}
