22e3d1a3e0d527cf41ec4853d6bae79bb9048ed6
slegrand <ut5svsxgc@relay.firefox.com>
2021-10-13 21:17:13 -0400

    Fixed basic arithmetic operations

--- files/library/maths/fixed.ts
+++ files/library/maths/fixed.ts
@@ -37,4 +37,23 @@ export class Fixed {
         return this.value_ >> SHIFT
     }
 
+    // addition between two fixed
+    public add(other: Fixed): Fixed {
+        return new Fixed(this.value_ + other.value_);
+    }
+
+    // substraction between two fixed
+    public sub(other: Fixed): Fixed {
+        return new Fixed(this.value_ - other.value_);
+    }
+
+    // multiply two fixed
+    public mul(other: Fixed): Fixed {
+        return new Fixed((this.value_ * other.value_) >> SHIFT);
+    }
+
+    // divide two fixed
+    public div(other: Fixed): Fixed {
+        return new Fixed(Math.floor(this.value_ / other.value_) << SHIFT);
+    }
 }
