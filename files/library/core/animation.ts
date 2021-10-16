bc80ccc8ce309d8d03d01a6f7586d87e87233e4e
slegrand <ut5svsxgc@relay.firefox.com>
2021-10-16 16:38:36 -0400

    Variable names change + make Display a generic member of IAnimation

--- files/library/core/animation.ts
+++ files/library/core/animation.ts
@@ -7,26 +7,33 @@
 
 //----- imports
 import { IStateTask } from "./manager";
+import { Display } from "./display";
+
 
 //----- class
 export abstract class IAnimation extends IStateTask {
+
     //----- members
-    private isAnimated_: boolean;       // =true when the animation is running
+    private animated_: boolean;         // =true when the animation is running
+
+    protected display_: Display;        // the display screen where the animation is shown
     protected frames_: number;          // number of frames rendered
 
 
     //----- methods
-    constructor(name: string) {
+    constructor(name: string, display: Display) {
         super(name);
 
-        this.isAnimated_ = false;
+        // set the vars
+        this.animated_ = false;
+        this.display_ = display;
         this.frames_ = 0;
     }
 
     // toggle the animation
     public toggle(): void {
-        this.isAnimated_ = !this.isAnimated_;
-        if ( !this.isAnimated_ ) {
+        this.animated_ = !this.animated_;
+        if ( !this.animated_ ) {
             console.log('Animation stopped.');
         }
         else {
@@ -36,12 +43,12 @@ export abstract class IAnimation extends IStateTask {
 
     // return the animation state
     public get isAnimated(): boolean {
-        return this.isAnimated_;
+        return this.animated_;
     }
 
     // update the animation
-    protected abstract update(timestamp: number): void;
+    protected abstract update(time: number|undefined): void;
 
     // render the animation on the screen
-    protected abstract render(timestamp: number): void;
+    protected abstract render(time: number|undefined): void;
 }
