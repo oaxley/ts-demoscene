8fc7729bdef6a76a3caeeec618b25f7a73d30108
slegrand <ut5svsxgc@relay.firefox.com>
2021-10-25 18:00:12 -0400

    ts-tunnel: main entry point for the animation

---
+++ files/effects/ts-tunnel/src/main.ts
@@ -0,0 +1,28 @@
+/*
+ * @file    main.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point for the Tunnel effect
+ */
+
+//----- imports
+import { StatesManager, States } from "library/core/manager"
+import { Display } from "library/core/display";
+import { Tunnel } from "./tunnel";
+
+
+//----- begin
+// retrieve the canvas element from the page
+let display = new Display(<HTMLCanvasElement> document.getElementById("output"));
+
+// create a new instance of the tunnel
+let tunnel = new Tunnel(display);
+
+// create states manager
+let manager = new StatesManager();
+
+// add a new transition
+manager.add({event: States.S_BEGIN, from: undefined, to: tunnel});
+
+// start
+manager.start();
