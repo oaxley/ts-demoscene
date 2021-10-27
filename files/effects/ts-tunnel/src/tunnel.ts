a6d994ce33dae76433dd5afa6a8f7090870ae5be
slegrand <ut5svsxgc@relay.firefox.com>
2021-10-26 21:44:48 -0400

    ts-tunnel: compute the effect and update the screen

--- files/effects/ts-tunnel/src/tunnel.ts
+++ files/effects/ts-tunnel/src/tunnel.ts
@@ -82,6 +82,63 @@ export class Tunnel extends IAnimation {
     protected update(timestamp: number): void {
         if (!this.isAnimated)
             return;
+
+        // retrieve the surface and texture data
+        let imgdata = this.display_.surface.data;
+        let texdata = this.texture_.data;
+
+        // constants
+        const tw = this.texture_.width;
+        const th = this.texture_.height;
+        const w = this.display_.width;
+        const h = this.display_.height;
+
+        const time = timestamp / 1000;
+
+        // compute the effect
+        let effectX = Math.floor(tw * 1.00 * time);
+        let effectY = Math.floor(th * 0.25 * time);
+
+        // camera movement effect
+        let cameraX = (w >> 1) + Math.floor((w >> 2) * Math.sin(time));
+        let cameraY = (h >> 1) + Math.floor((h >> 2) * Math.sin(time * 2.0));
+
+        // go through all the pixels
+        for (let y = 0; y < h; y++) {
+            // screen offset
+            let offset = y * w;
+
+            for (let x = 0; x < w; x++) {
+                // texture offset
+                let t_off = (y + cameraY) * (w << 1) + (x + cameraX)
+
+                // texture coordinates
+                let tx = Math.floor(this.distance_[t_off] + effectX) % tw;
+                let ty = Math.floor(this.angle_[t_off] + effectY) % th;
+
+                // adjust ty in case it's negative
+                while (ty < 0) {
+                    ty += th;
+                }
+
+                // shade value
+                let shade = this.shade_[t_off] / 255;
+
+                // source and destination offset
+                let d_addr = (offset + x) << 2;
+                let s_addr = (ty * tw + tx) << 2;
+
+                // copy the pixel back to the screen
+                imgdata.data[d_addr + 0] = Math.floor(texdata.data[s_addr + 0] * shade);
+                imgdata.data[d_addr + 1] = Math.floor(texdata.data[s_addr + 1] * shade);
+                imgdata.data[d_addr + 2] = Math.floor(texdata.data[s_addr + 2] * shade);
+                imgdata.data[d_addr + 3] = Math.floor(texdata.data[s_addr + 3] * 1.0);
+            }
+        }
+
+        // copy back the data on the surface
+        this.display_.surface.data = imgdata;
+
     }
 
     // render the animation on the screen
