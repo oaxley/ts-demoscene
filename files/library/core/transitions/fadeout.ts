331ca9c68f0704502d76a926a38c467aa1d519b1
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-06 11:06:19 -0400

    Add Fade-Out effect

---
+++ files/library/core/transitions/fadeout.ts
@@ -0,0 +1,28 @@
+/*
+ * @file    fadein.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Fade-Out transition effect
+ */
+
+//----- imports
+import { ITransition } from "../transition";
+import { Surface } from "../surface";
+import { Rect } from "../interfaces";
+import { lerp } from "library/maths/utils";
+
+
+//----- class
+export class FadeOut extends ITransition {
+
+    //----- methods
+    constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+        super('fadeout', display, refimage, delay, viewport);
+    }
+
+    // compute the new values for RGBA
+    protected compute(values: number[]): number[] {
+        let [ r, g, b, a, t ] = values;
+        return [ lerp(r, 0, t), lerp(g, 0, t), lerp(b, 0, t), lerp(a, 0, t) ];
+    }
+}
