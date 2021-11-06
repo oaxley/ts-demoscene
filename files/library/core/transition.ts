54f69f64352785e3a3cd27e8fe4914fe89361b1b
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-06 10:40:58 -0400

    Complete the update methods function

--- files/library/core/transition.ts
+++ files/library/core/transition.ts
@@ -40,6 +40,50 @@ export abstract class ITransition extends IStateTask {
 
     // update the animation
     public update(time: number): States {
+        if (this.start_ == -1) {
+            this.start_ = time;
+        }
+
+        // display parameters
+        let dstdata = this.display_.data;
+        const dw = this.display_.width;
+
+        // reference image parameters
+        let srcdata = this.refimage_.data
+
+        // compute the current time until the end
+        let t = (time - this.start_) / this.delay_;
+        if (t >= 1.0) {
+            return States.S_END;
+        }
+
+        // effect takes place only on the viewport
+        for (let y = this.viewport_.y; y < (this.viewport_.y + this.viewport_.h); y++) {
+            let offset = y * dw;
+
+            for (let x = this.viewport_.x; x < (this.viewport_.x + this.viewport_.w); x++) {
+
+                let addr = (offset + x) << 2;
+
+                // retrieve the rgba parameters
+                let r = srcdata.data[addr + 0];
+                let g = srcdata.data[addr + 1];
+                let b = srcdata.data[addr + 2];
+                let a = srcdata.data[addr + 3];
+
+                // compute the effect
+                [r, g, b, a] = this.compute([r, g, b, a, t]);
+
+                // store the new values
+                dstdata.data[addr + 0] = r;
+                dstdata.data[addr + 1] = g;
+                dstdata.data[addr + 2] = b;
+                dstdata.data[addr + 3] = a;
+            }
+        }
+
+        this.display_.data = dstdata;
+
         return States.S_RUNNING;
     }
 
