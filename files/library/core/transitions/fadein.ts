630a4eb4f0e8ed01ff622f6492741c90ba2c537d
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-06 10:49:29 -0400

    Add Fade-In effect

---
+++ files/library/core/transitions/fadein.ts
@@ -0,0 +1,28 @@
+/*
+ * @file    fadein.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Fade-In transition effect
+ */
+
+//----- imports
+import { ITransition } from "../transition";
+import { Surface } from "../surface";
+import { Rect } from "../interfaces";
+import { lerp } from "library/maths/utils";
+
+
+//----- class
+export class FadeIn extends ITransition {
+
+    //----- methods
+    constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+        super('fadein', display, refimage, delay, viewport);
+    }
+
+    // compute the new values for RGBA
+    protected compute(values: number[]): number[] {
+        let [ r, g, b, a, t ] = values;
+        return [ r, g, b, a] = [ lerp(0, r, t), lerp(0, g, t), lerp(0, b, t), lerp(0, a, t) ];
+    }
+}
