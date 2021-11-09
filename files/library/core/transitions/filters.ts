db45fade0bc6ebe942f45e4b6085f3c8e9e4190a
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-09 18:26:35 -0500

    Add Sepia filter

---
+++ files/library/core/transitions/filters.ts
@@ -0,0 +1,40 @@
+/*
+ * @file    white.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   White In/Out transition effect
+ */
+
+//----- imports
+import { ITransition } from "../transition";
+import { Surface } from "../surface";
+import { Rect } from "../interfaces";
+import { lerp } from "library/maths/utils";
+
+
+//----- class
+export namespace Filter {
+
+    // Sepia Filter
+    export class Sepia extends ITransition {
+        //----- methods
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+            super('sepia', display, refimage, delay, viewport);
+        }
+
+        // compute the new values for RGBA
+        protected compute(values: number[]): number[] {
+            let [ r, g, b, a, t ] = values;
+            let tr = 0.393 * r + 0.769 * g + 0.189 * b;
+            let tg = 0.349 * r + 0.686 * g + 0.168 * b;
+            let tb = 0.272 * r + 0.534 * g + 0.131 * b;
+
+            r = (tr > 255) ? 255 : tr;
+            g = (tg > 255) ? 255 : tg;
+            b = (tb > 255) ? 255 : tb;
+
+            return [ r, g, b, a ];
+        }
+    }
+
+}
