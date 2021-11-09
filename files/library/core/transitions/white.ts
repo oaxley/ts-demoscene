d2368c79b1ad8efa595370faeb0be28aa12c94c8
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-08 21:20:05 -0500

    Create namespace for White effect

--- files/library/core/transitions/white.ts
+++ files/library/core/transitions/white.ts
@@ -13,16 +13,21 @@ import { lerp } from "library/maths/utils";
 
 
 //----- class
-export class FadeWhite extends ITransition {
+export namespace White {
 
-    //----- methods
-    constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
-        super('fade-white', display, refimage, delay, viewport);
+    // make the transition from the reference image to white
+    export class In extends ITransition {
+
+        //----- methods
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+            super('fade-white', display, refimage, delay, viewport);
+        }
+
+        // compute the new values for RGBA
+        protected compute(values: number[]): number[] {
+            let [ r, g, b, a, t ] = values;
+            return [ lerp(r, 255, t), lerp(g, 255, t), lerp(b, 255, t), a ];
+        }
     }
 
-    // compute the new values for RGBA
-    protected compute(values: number[]): number[] {
-        let [ r, g, b, a, t ] = values;
-        return [ lerp(r, 255, t), lerp(g, 255, t), lerp(b, 255, t), a ];
-    }
 }
