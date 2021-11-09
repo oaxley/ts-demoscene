a58837ea37ba9faaa75e78de694f9336be1bdec5
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-08 21:28:53 -0500

    Add namespace and gray-out effect

--- files/library/core/transitions/gray.ts
+++ files/library/core/transitions/gray.ts
@@ -13,17 +13,38 @@ import { lerp } from "library/maths/utils";
 
 
 //----- class
-export class FadeGray extends ITransition {
+export namespace Gray {
 
-    //----- methods
-    constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
-        super('fade-gray', display, refimage, delay, viewport);
+    // make the transition from the reference image to gray
+    export class In extends ITransition {
+
+        //----- methods
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+            super('gray-in', display, refimage, delay, viewport);
+        }
+
+        // compute the new values for RGBA
+        protected compute(values: number[]): number[] {
+            let [ r, g, b, a, t ] = values;
+            let lum = Math.floor(0.299 * r + 0.587 * g + 0.114 * b);
+            return [ lerp(r, lum, t), lerp(g, lum, t), lerp(b, lum, t), a ];
+        }
     }
 
-    // compute the new values for RGBA
-    protected compute(values: number[]): number[] {
-        let [ r, g, b, a, t ] = values;
-        let lum = Math.floor(0.299 * r + 0.587 * g + 0.114 * b);
-        return [ lerp(r, lum, t), lerp(g, lum, t), lerp(b, lum, t), a ];
+    // make the transition from gray to the reference image
+    export class Out extends ITransition {
+
+        //----- methods
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+            super('gray-out', display, refimage, delay, viewport);
+        }
+
+        // compute the new values for RGBA
+        protected compute(values: number[]): number[] {
+            let [ r, g, b, a, t ] = values;
+            let lum = Math.floor(0.299 * r + 0.587 * g + 0.114 * b);
+            return [ lerp(lum, r, t), lerp(lum, g, t), lerp(lum, b, t), a ];
+        }
     }
+
 }
