a79d2dc1aa201dde20e9351d984db93cabebc809
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-11 10:31:44 -0500

    ts-tunnel: use StatesManager singleton

--- files/effects/ts-tunnel/src/main.ts
+++ files/effects/ts-tunnel/src/main.ts
@@ -19,7 +19,7 @@ let display = new Display(<HTMLCanvasElement> document.getElementById("output"))
 let tunnel = new Tunnel(display);
 
 // create states manager
-let manager = new StatesManager();
+let manager = StatesManager.getInstance();
 
 // add a new transition
 manager.add({event: States.S_BEGIN, from: undefined, to: tunnel});
