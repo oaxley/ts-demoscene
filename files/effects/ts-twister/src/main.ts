d065de07ae263b793b3aa2e2b758b17fd6ce1e1b
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-11 10:35:08 -0500

    ts-twister: use StatesManager singleton

--- files/effects/ts-twister/src/main.ts
+++ files/effects/ts-twister/src/main.ts
@@ -18,7 +18,7 @@ let display = new Display(<HTMLCanvasElement> document.getElementById("output"))
 let twister = new Twister(display);
 
 // create states manager
-let manager = new StatesManager();
+let manager = StatesManager.getInstance();
 
 // add a new transition
 manager.add({event: States.S_BEGIN, from: undefined, to: twister});
