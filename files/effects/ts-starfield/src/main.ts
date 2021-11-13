cf452ff1165c95442e7e5081ef2e55f7b4182f51
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-12 19:06:32 -0500

    ts-starfield: main entry point for the animation

---
+++ files/effects/ts-starfield/src/main.ts
@@ -0,0 +1,25 @@
+/*
+ * @file    main.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point for the Starfield effect
+ */
+
+//----- imports
+import { StatesManager, States } from "library/core/manager";
+import { Display } from "library/core/display";
+import { Starfield } from "./starfield";
+
+//----- begin
+// retrieve the canvas element from the page
+let display = new Display(<HTMLCanvasElement> document.getElementById("output"));
+
+// create a new instance of the Starfield
+let starfield = new Starfield(display);
+
+// retrieve the instance of the states manager and add a new state transition
+let manager = StatesManager.getInstance();
+manager.add({event: States.S_BEGIN, from: undefined, to: starfield});
+
+// start the animation
+manager.start();
