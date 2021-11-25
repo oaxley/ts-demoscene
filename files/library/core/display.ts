9e56cbceaf097f29ccc839102778008ab572df86
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-24 22:56:15 -0500

    Rework Display with new Surface class

--- files/library/core/display.ts
+++ files/library/core/display.ts
@@ -6,62 +6,53 @@
  */
 
 //----- imports
+import e from "express";
 import { Surface } from "library/gfx/surface";
+
+
+//----- globals
+
+// enum for snapshot function
+export enum EDisplaySource {
+    E_DISPLAY = 0,
+    E_BUFFER
+}
 
 
 //----- class
 export class Display {
     //----- members
-    private screen_: HTMLCanvasElement;
-    private context_: CanvasRenderingContext2D;
-    private surface_: Surface;
-    private snapshot_: Surface;
+    private screen_  : HTMLCanvasElement;               // display screen canvas
+    private context_ : CanvasRenderingContext2D;        // display screen context
+    private width_   : number;                          // display width
+    private height_  : number;                          // display height
+
+    private surface_ : Surface;                         // back-buffer surface
+    private snapshot_: Surface;                         // snapshot surface
 
 
     //----- methods
     constructor(canvas: HTMLCanvasElement) {
+        // set the vars
         this.screen_  = canvas;
         this.context_ = canvas.getContext("2d")!;
+        this.width_   = canvas.width;
+        this.height_  = canvas.height;
+
         this.surface_ = new Surface({width: canvas.width, height: canvas.height});
-        this.snapshot_= new Surface({width: canvas.width, height: canvas.height});
+        this.snapshot_= new Surface();
     }
+
+    //----- accessors
 
     // accessor to get the display width
     public get width(): number {
-        return this.screen_.width;
+        return this.width_;
     }
 
     // accessor to get the display height
     public get height(): number {
-        return this.screen_.height;
-    }
-
-    // draw the display back surface on the canvas element
-    public draw() {
-        this.context_.drawImage(this.surface_.canvas, 0, 0);
-    }
-
-    // retrieve the display back surface
-    public get surface(): Surface {
-        return this.surface_;
-    }
-
-    // retrieve the display snaphot surface
-    public get snapshot(): Surface {
-        return this.snapshot_;
-    }
-
-    // take a snapshot of the display screen/back-surface
-    // true = back-surface
-    // false = screen
-    public takeSnapshot(toggle: boolean) {
-        if (toggle) {
-            // copy the back-surface
-            this.snapshot_.copy(this.surface_);
-        } else {
-            // copy the screen surface
-            this.snapshot_.context.drawImage(this.screen, 0, 0);
-        }
+        return this.height_;
     }
 
     // retrieve the screen canvas element
@@ -69,13 +60,40 @@ export class Display {
         return this.screen_;
     }
 
+    // retrieve the screen context
+    public get context(): CanvasRenderingContext2D {
+        return this.context_;
+    }
+
+    // retrieve the display back-buffer
+    public get surface(): Surface {
+        return this.surface_;
+    }
+
+    // retrieve the snaphot surface
+    public get snapshot(): Surface {
+        return this.snapshot_;
+    }
+
+    //----- functions
+
+    // draw the back-buffer on the screen canvas
+    public draw() {
+        this.context_.putImageData(this.surface_.image, 0, 0);
+    }
+
+    // take a snapshot of the display screen/back-buffer
+    public takeSnapshot(value: EDisplaySource = EDisplaySource.E_BUFFER): void {
+        if (value == EDisplaySource.E_DISPLAY) {
+            let imgdata = this.context_.getImageData(0, 0, this.width_, this.height_);
+            this.snapshot_.image = imgdata;
+        } else {
+            this.snapshot_.copy(this.surface_);
+        }
+    }
+
     // clear the screen
     public clear() {
         this.context_.clearRect(0, 0, this.screen_.width, this.screen_.height);
-    }
-
-    // load an image on the display back Surface
-    public loadImage(image: HTMLImageElement) {
-        this.surface_.context.drawImage(image, 0, 0);
     }
 }
