4acf8a5c3d9ee1bf14137707804d2562edb62c1f
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-28 16:08:54 -0500

    Rewrite Color Transition with new Surface methods

--- files/library/gfx/icolortrans.ts
+++ files/library/gfx/icolortrans.ts
@@ -2,27 +2,29 @@
  * @file    transition.ts
  * @author  Sebastien LEGRAND
  *
- * @file    Abstract class to define transition effects
+ * @file    Abstract class to define color transition effects
  */
 
 //----- imports
-import { IStateTask, States } from "./manager";
+import { Rect } from "library/core/interfaces";
+import { IStateTask, States } from "library/core/manager";
 import { Surface } from "library/gfx/surface";
-import { Rect } from "./interfaces";
+import { Viewport } from "./viewport";
 
 
 //----- class
-export abstract class ITransition extends IStateTask {
+export abstract class IColorTransition extends IStateTask {
 
     //----- members
     protected display_ : Surface;       // the surface where the animation will be shown
     protected refimage_: Surface;       // the reference image
-    protected viewport_: Rect;          // viewport for the animation
     protected delay_   : number;        // maximum time to play the effect
     protected start_   : number;        // start time of the effect
+    private viewport_  : Viewport;      // effect viewport
+
 
     //----- methods
-    constructor(name: string, display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+    constructor(name: string, display: Surface, refimage: Surface, delay: number, viewport?: Viewport) {
         super(name);
 
         // set the vars
@@ -32,7 +34,7 @@ export abstract class ITransition extends IStateTask {
         this.start_   = -1;
 
         if (viewport === undefined) {
-            this.viewport_ = { x: 0, y: 0, w: display.width, h: display.height}
+            this.viewport_ = new Viewport();
         } else {
             this.viewport_ = viewport;
         }
@@ -40,16 +42,10 @@ export abstract class ITransition extends IStateTask {
 
     // update the animation
     public update(time: number): States {
+        // initialize the start time
         if (this.start_ == -1) {
             this.start_ = time;
         }
-
-        // display parameters
-        let dstdata = this.display_.data;
-        const dw = this.display_.width;
-
-        // reference image parameters
-        let srcdata = this.refimage_.data
 
         // compute the current time until the end
         let t = (time - this.start_) / this.delay_;
@@ -57,33 +53,27 @@ export abstract class ITransition extends IStateTask {
             return States.S_END;
         }
 
-        // effect takes place only on the viewport
-        for (let y = this.viewport_.y; y < (this.viewport_.y + this.viewport_.h); y++) {
-            let offset = y * dw;
-
-            for (let x = this.viewport_.x; x < (this.viewport_.x + this.viewport_.w); x++) {
-
-                let addr = (offset + x) << 2;
-
-                // retrieve the rgba parameters
-                let r = srcdata.data[addr + 0];
-                let g = srcdata.data[addr + 1];
-                let b = srcdata.data[addr + 2];
-                let a = srcdata.data[addr + 3];
-
-                // compute the effect
-                [r, g, b, a] = this.compute([r, g, b, a, t]);
-
-                // store the new values
-                dstdata.data[addr + 0] = Math.floor(r);
-                dstdata.data[addr + 1] = Math.floor(g);
-                dstdata.data[addr + 2] = Math.floor(b);
-                dstdata.data[addr + 3] = Math.floor(a);
-            }
+        // viewport parameters
+        let values: Rect = {x: 0, y: 0, w: this.display_.width, h: this.display_.height};
+        if (this.viewport_.isEnabled) {
+            values = this.viewport_.view;
         }
 
-        this.display_.data = dstdata;
+        // effect takes place only within the viewport
+        const dw = this.display_.width;
+        for (let y = values.y; y < (values.y + values.h); y++) {
+            let offset = y * dw;
 
+            this.refimage_.address = offset + values.x;
+            this.display_.address  = offset + values.x;
+
+            for (let x = values.x; x < (values.x + values.w); x++) {
+                // compute the effect
+                let rgba = this.refimage_.streamW;
+                rgba = this.compute(t, rgba);
+                this.display_.streamW = rgba;
+            }
+        }
         return States.S_RUNNING;
     }
 
@@ -97,5 +87,5 @@ export abstract class ITransition extends IStateTask {
     }
 
     // this should be implementer by the effect
-    protected abstract compute(values: number[]): number[];
+    protected abstract compute(time: number, value: number): number;
 }
