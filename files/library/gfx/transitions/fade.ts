d518a1f23a9cf71156b239d191d03acbeafc0af3
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-29 20:58:16 -0500

    Rewrite Fade color transition

--- files/library/gfx/transitions/fade.ts
+++ files/library/gfx/transitions/fade.ts
@@ -6,42 +6,43 @@
  */
 
 //----- imports
-import { ITransition } from "../transition";
 import { Surface } from "library/gfx/surface";
-import { Rect } from "../interfaces";
+import { RGBA } from "library/color/RGBA";
 import { lerp } from "library/maths/utils";
+import { IColorTransition } from "../icolortrans";
+import { Viewport } from "../viewport";
 
 
 //----- class
 export namespace Fade {
 
     // fade-in effect
-    export class In extends ITransition {
+    export class In extends IColorTransition {
 
         //----- methods
-        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Viewport) {
             super('fadein', display, refimage, delay, viewport);
         }
 
         // compute the new values for RGBA
-        protected compute(values: number[]): number[] {
-            let [ r, g, b, a, t ] = values;
-            return [ lerp(0, r, t), lerp(0, g, t), lerp(0, b, t), a ];
+        protected compute(time: number, value: number): number {
+            let [ r, g, b, a ] = RGBA.fromUInt32(value);
+            return RGBA.toUInt32(lerp(0, r, time), lerp(0, g, time), lerp(0, b, time), a);
         }
     }
 
     // fade-out effect
-    export class Out extends ITransition {
+    export class Out extends IColorTransition {
 
         //----- methods
-        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Viewport) {
             super('fadeout', display, refimage, delay, viewport);
         }
 
         // compute the new values for RGBA
-        protected compute(values: number[]): number[] {
-            let [ r, g, b, a, t ] = values;
-            return [ lerp(r, 0, t), lerp(g, 0, t), lerp(b, 0, t), a ];
+        protected compute(time: number, value: number): number {
+            let [ r, g, b, a ] = RGBA.fromUInt32(value);
+            return  RGBA.toUInt32(lerp(r, 0, time), lerp(g, 0, time), lerp(b, 0, time), a);
         }
     }
 
