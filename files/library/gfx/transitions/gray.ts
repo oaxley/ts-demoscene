876a43ccd02419d7671d127f8c81763fb917b529
slegrand <ut5svsxgc@relay.firefox.com>
2021-11-29 21:20:19 -0500

    Rewrite Gray color transition

--- files/library/gfx/transitions/gray.ts
+++ files/library/gfx/transitions/gray.ts
@@ -6,44 +6,45 @@
  */
 
 //----- imports
-import { ITransition } from "../transition";
 import { Surface } from "library/gfx/surface";
-import { Rect } from "../interfaces";
+import { RGBA } from "library/color/RGBA";
 import { lerp } from "library/maths/utils";
+import { IColorTransition } from "../icolortrans";
+import { Viewport } from "../viewport";
 
 
 //----- class
 export namespace Gray {
 
     // make the transition from the reference image to gray
-    export class In extends ITransition {
+    export class In extends IColorTransition {
 
         //----- methods
-        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Viewport) {
             super('gray-in', display, refimage, delay, viewport);
         }
 
         // compute the new values for RGBA
-        protected compute(values: number[]): number[] {
-            let [ r, g, b, a, t ] = values;
+        protected compute(time: number, value: number): number {
+            let [ r, g, b, a ] = RGBA.fromUInt32(value);
             let lum = Math.floor(0.299 * r + 0.587 * g + 0.114 * b);
-            return [ lerp(r, lum, t), lerp(g, lum, t), lerp(b, lum, t), a ];
+            return RGBA.toUInt32(lerp(r, lum, time), lerp(g, lum, time), lerp(b, lum, time), a);
         }
     }
 
     // make the transition from gray to the reference image
-    export class Out extends ITransition {
+    export class Out extends IColorTransition {
 
         //----- methods
-        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Rect) {
+        constructor(display: Surface, refimage: Surface, delay: number, viewport?: Viewport) {
             super('gray-out', display, refimage, delay, viewport);
         }
 
         // compute the new values for RGBA
-        protected compute(values: number[]): number[] {
-            let [ r, g, b, a, t ] = values;
+        protected compute(time: number, value: number): number {
+            let [ r, g, b, a ] = RGBA.fromUInt32(value);
             let lum = Math.floor(0.299 * r + 0.587 * g + 0.114 * b);
-            return [ lerp(lum, r, t), lerp(lum, g, t), lerp(lum, b, t), a ];
+            return RGBA.toUInt32(lerp(lum, r, time), lerp(lum, g, time), lerp(lum, b, time), a);
         }
     }
 
