8bb498fe1b8352f638b344e0b977dc47a51cb4bb
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-02 17:46:15 -0500

    ts-smoke: main entry point for the animation

---
+++ files/effects/ts-smoke/src/main.ts
@@ -0,0 +1,28 @@
+/*
+ * @file    main.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point for the Flames effect
+ */
+
+//----- imports
+import { StatesManager, States } from "library/core/manager"
+import { Display } from "library/core/display";
+import { Smoke } from "./smoke";
+
+
+//----- begin
+// retrieve the canvas element from the page
+let display = new Display(<HTMLCanvasElement> document.getElementById("output"));
+
+// create a new instance of the effect
+let smoke = new Smoke(display);
+
+// create states manager
+let manager = StatesManager.getInstance();
+
+// add a new transition
+manager.add({event: States.S_BEGIN, from: undefined, to: smoke});
+
+// start
+manager.start();
