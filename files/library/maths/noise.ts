4164b063378f7a62da593d6e46cffb973e9f3fe9
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-01 21:03:23 -0500

    Add simple 1D noise function

---
+++ files/library/maths/noise.ts
@@ -0,0 +1,59 @@
+/*
+ * @file    noise.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Random noise generator
+ */
+
+//----- imports
+import { lerp } from "./utils";
+import { Mersenne } from "./mersenne";
+
+
+//----- globals
+const MAX_VERTICES = 512
+
+
+//----- class
+export namespace Noise {
+
+    export class Noise1D {
+        //----- members
+        private vertices_: Array<number>;
+
+
+        //----- methods
+        constructor(seed?: number) {
+
+            let mt = new Mersenne();
+            this.vertices_ = new Array(MAX_VERTICES + 1);
+
+            // generate the values
+            if (seed === undefined) {
+                seed = performance.now();
+            }
+
+            mt.initialize(seed);
+
+            for (let i = 0; i <= MAX_VERTICES; i++) {
+                this.vertices_[i] = mt.rand();
+            }
+        }
+
+        // smoothstep function
+        private smoothStep(a: number, b: number, t: number): number {
+            t = t * t * (3 - 2 * t);
+            return lerp(a, b, t);
+        }
+
+        // evaluate the noise at position x
+        public eval(x: number): number {
+            let xi = Math.floor(x);
+            let xmin = xi % MAX_VERTICES;
+            let xmax = (xmin == MAX_VERTICES - 1) ? 0 : xmin + 1;
+            let t = x - xi;
+
+            return this.smoothStep(this.vertices_[xmin], this.vertices_[xmax], t);
+        }
+    }
+}
