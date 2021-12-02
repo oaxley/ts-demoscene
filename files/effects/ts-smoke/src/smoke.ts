4750e6a730599b2b56eefd4424d0a1ee89aae7b1
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-02 17:42:46 -0500

    ts-smoke: basic animation framework

---
+++ files/effects/ts-smoke/src/smoke.ts
@@ -0,0 +1,78 @@
+/*
+ * @file    smoke.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Smoke effect in Typescript
+ */
+
+//----- imports
+import { IAnimation } from "library/core/animation";
+import { States } from "library/core/manager";
+import { Display } from "library/core/display";
+
+
+//----- globals
+
+
+//----- class
+export class Smoke extends IAnimation {
+
+    //----- members
+
+
+    //----- methods
+    // constructor
+    constructor(display: Display) {
+        super('smoke', display);
+
+        // set the vars
+    }
+
+
+    // update the animation
+    protected update(time?: number): void {
+        if (!this.isAnimated)
+            return;
+
+    }
+
+    // render the animation on the screen
+    protected render(time?: number): void {
+        if (!this.isAnimated)
+            return;
+
+        // flip the back-buffer onto the screen
+        this.display_.draw();
+
+        // increment the number of frames
+        this.frames_++;
+    }
+
+    // setup function
+    public setup(): void {
+        // toggle the animation
+        this.toggle();
+
+        // set the click handler to pause the animation
+        window.onclick = () => {
+            this.toggle();
+        }
+
+        console.log("Starting Smoke animation.");
+    }
+
+    // cleanup function
+    public cleanup(): void {
+    }
+
+    // run the animation
+    public run(time: number|undefined): States {
+
+        // update & render the effect
+        this.update(time);
+        this.render(time);
+
+        // this animation will run indefinitely
+        return States.S_RUNNING;
+    }
+}
