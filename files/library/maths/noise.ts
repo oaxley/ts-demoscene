ffe800e2ed622f49881b173da7eda924e1ac4781
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-05 14:20:56 -0500

    Reduce noise maximum vertices + rewrite constructor

--- files/library/maths/noise.ts
+++ files/library/maths/noise.ts
@@ -11,7 +11,7 @@ import { Mersenne } from "./mersenne";
 
 
 //----- globals
-const MAX_VERTICES = 512;
+const MAX_VERTICES = 256;
 const MAX_VERTICES_MASK = MAX_VERTICES - 1;
 
 
@@ -22,21 +22,18 @@ export namespace Noise {
         //----- members
         private vertices_: Array<number>;
 
-
         //----- methods
         constructor(seed?: number) {
+            // create a 1D array of 512 elements
+            this.vertices_ = new Array(MAX_VERTICES);
 
+            // setup the Mersenne Twister random generator
+            seed = (seed === undefined) ? performance.now() : seed;
             let mt = new Mersenne();
-            this.vertices_ = new Array(MAX_VERTICES + 1);
-
-            // generate the values
-            if (seed === undefined) {
-                seed = performance.now();
-            }
-
             mt.initialize(seed);
 
-            for (let i = 0; i <= MAX_VERTICES; i++) {
+            // fill the grid
+            for (let i = 0; i < MAX_VERTICES; i++) {
                 this.vertices_[i] = mt.rand();
             }
         }
