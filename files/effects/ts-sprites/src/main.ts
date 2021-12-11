e6938faf27105eabc9ddc6fadeddf2300907d79d
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-11 11:16:09 -0500

    ts-sprites: main entry point for the animation

---
+++ files/effects/ts-sprites/src/main.ts
@@ -0,0 +1,28 @@
+/*
+ * @file    main.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point for the Sprites effect
+ */
+
+//----- imports
+import { Display } from "library/core/display";
+import { StatesManager, States } from "library/core/manager"
+import { Sprites } from "./sprites";
+
+
+//----- begin
+// retrieve the canvas element from the page
+let display = new Display(<HTMLCanvasElement> document.getElementById("output"));
+
+// create a new instance of the effect
+let effect = new Sprites(display);
+
+// create states manager
+let manager = StatesManager.getInstance();
+
+// add a new transition
+manager.add({event: States.S_BEGIN, from: undefined, to: effect});
+
+// start
+manager.start();
