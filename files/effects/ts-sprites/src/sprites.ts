61452238d5fc7a819081d220ec35e97273e40cf0
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-11 11:57:14 -0500

    ts-sprites: create particules

--- files/effects/ts-sprites/src/sprites.ts
+++ files/effects/ts-sprites/src/sprites.ts
@@ -10,16 +10,27 @@ import { IAnimation } from "library/core/animation";
 import { States } from "library/core/manager";
 import { Display } from "library/core/display";
 import { Surface } from "library/gfx/surface";
+import { Point2D } from "library/core/interfaces";
+import { Vector2D } from "library/maths/vector2d";
 
 
 //----- globals
+const NUM_PARTICULES = 100;
 
+
+//----- interfaces
+interface Particule {
+    p: Point2D;
+    inc: Vector2D;
+}
 
 //----- class
 export class Sprites extends IAnimation {
 
     //----- members
     private sprite_: Surface;
+    private particules_: Particule[];       // particule elements
+
 
     //----- methods
     // constructor
@@ -28,6 +39,29 @@ export class Sprites extends IAnimation {
 
         // set the vars
         this.sprite_ = new Surface();
+
+        // create the particules
+        this.particules_ = [];
+        this.createParticules();
+    }
+
+    private createParticules(): void {
+        for (let i = 0; i < NUM_PARTICULES; i++) {
+
+            // find a random point
+            let p: Point2D = {
+                x: Math.floor(Math.random() * this.display_.width),
+                y: Math.floor(Math.random() * this.display_.height)
+            };
+
+            // increment is between -1 and 1
+            let v: Vector2D = new Vector2D(
+                Math.floor(-1 + Math.random() * 2),
+                Math.floor(-1 + Math.random() * 2)
+            );
+
+            this.particules_.push({p: p, inc: v});
+        }
     }
 
     // update the animation
