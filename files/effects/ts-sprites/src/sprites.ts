a3c40b190a2b5bcc05da18f793399659ae4e3561
slegrand <ut5svsxgc@relay.firefox.com>
2021-12-11 14:11:45 -0500

    ts-sprites: create animation

--- files/effects/ts-sprites/src/sprites.ts
+++ files/effects/ts-sprites/src/sprites.ts
@@ -12,6 +12,7 @@ import { Display } from "library/core/display";
 import { Surface } from "library/gfx/surface";
 import { Point2D } from "library/core/interfaces";
 import { Vector2D } from "library/maths/vector2d";
+import { RGBA } from "library/color/RGBA";
 
 
 //----- globals
@@ -56,8 +57,8 @@ export class Sprites extends IAnimation {
 
             // increment is between -1 and 1
             let v: Vector2D = new Vector2D(
-                Math.floor(-1 + Math.random() * 2),
-                Math.floor(-1 + Math.random() * 2)
+                -1 + Math.random() * 2,
+                -1 + Math.random() * 2
             );
 
             this.particules_.push({p: p, inc: v});
@@ -69,12 +70,61 @@ export class Sprites extends IAnimation {
         if (!this.isAnimated) {
             return;
         }
+
+        // update the particules
+        for (let i = 0; i < this.particules_.length; i++) {
+            // update the X value
+            this.particules_[i].p.x += this.particules_[i].inc.x;
+            if ((this.particules_[i].p.x > this.display_.width) || (this.particules_[i].p.x < 0)) {
+                this.particules_[i].inc.x *= -1;
+                this.particules_[i].p.x += this.particules_[i].inc.x;
+            }
+
+            // update the Y value
+            this.particules_[i].p.y += this.particules_[i].inc.y;
+            if ((this.particules_[i].p.y > this.display_.height) || (this.particules_[i].p.y < 0)) {
+                this.particules_[i].inc.y *= -1;
+                this.particules_[i].p.y += this.particules_[i].inc.y;
+            }
+        }
     }
 
     // render the animation on the screen
     protected render(time?: number): void {
         if (!this.isAnimated) {
             return;
+        }
+
+        let s = this.display_.surface;
+        s.clear();
+
+        let c = new RGBA(255, 255, 255);
+        for (let i = 0; i < this.particules_.length; i++) {
+            let x1 = Math.floor(this.particules_[i].p.x);
+            let y1 = Math.floor(this.particules_[i].p.y);
+
+            for (let j = 0; j < this.particules_.length; j++) {
+
+                if (i == j) {
+                    continue;
+                }
+
+                let x2 = Math.floor(this.particules_[j].p.x);
+                let y2 = Math.floor(this.particules_[j].p.y);
+
+                let xd = (x1 - x2);
+                let yd = (y1 - y2);
+                let d = Math.sqrt(xd * xd + yd * yd);
+
+                if ( d < 50 ) {
+                    s.line({x: x1, y: y1}, {x: x2, y: y2}, c);
+                } else {
+                    s.setPixel(x1  , y1  , c);
+                    s.setPixel(x1+1, y1  , c);
+                    s.setPixel(x1  , y1+1, c);
+                    s.setPixel(x1+1, y1+1, c);
+                }
+            }
         }
 
         // flip the back-buffer onto the screen
