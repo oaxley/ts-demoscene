21aad6964544e05247ce0a13b96529c43646a7ca
slegrand <ut5svsxgc@relay.firefox.com>
2022-01-06 18:09:04 -0500

    ts-noise: main entry point for the ImgNoise effect

---
+++ files/effects/ts-noise/src/main.ts
@@ -0,0 +1,27 @@
+/*
+ * @file    main.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point for the Noise effect
+ */
+
+//----- imports
+import { StatesManager, States } from "library/core/manager"
+import { Display } from "library/core/display";
+import { ImgNoise} from "./noise";
+
+//----- begin
+// retrieve the canvas element from the page
+let display = new Display(<HTMLCanvasElement> document.getElementById("output"));
+
+// create a new instance of the ImgNoise
+let moire = new ImgNoise(display);
+
+// create states manager
+let manager = StatesManager.getInstance();
+
+// add a new transition
+manager.add({event: States.S_BEGIN, from: undefined, to: moire});
+
+// start
+manager.start();
