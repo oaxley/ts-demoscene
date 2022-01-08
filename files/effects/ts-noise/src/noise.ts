5ac895be42922928d8def2fd6d944ad16c9bba65
slegrand <ut5svsxgc@relay.firefox.com>
2022-01-07 21:38:10 -0500

    ts-noise: implement the effect

--- files/effects/ts-noise/src/noise.ts
+++ files/effects/ts-noise/src/noise.ts
@@ -21,12 +21,17 @@ export class ImgNoise extends IAnimation {
     private image_: Surface;
     private noiseMap_: Array<number>;
 
+    private threshold_: number;
+    private increment_: number;
+
     //----- methods
     // constructor
     constructor(display: Display) {
         super('imgnoise', display);
 
         this.image_ = new Surface();
+        this.threshold_ = 1.0;
+        this.increment_ = 0.005;
 
         // build the noise map
         let noise = new Noise.Noise2D();
@@ -37,6 +42,43 @@ export class ImgNoise extends IAnimation {
     protected update(time?: number): void {
         if (!this.isAnimated)
             return;
+
+        // retrieve src and dst image data
+        let src_data = this.image_.image;
+        let dst_data = this.display_.surface.image;
+
+        // go through the whole screen to trigger pixels
+        for (let y = 0; y < this.display_.height; y++) {
+            let offset = y * this.display_.width;
+
+            for (let x = 0; x < this.display_.width; x++) {
+                let k = this.noiseMap_[ y * this.display_.width + x ];
+
+                if ( k >= this.threshold_ ) {
+                    dst_data.data[(offset << 2) + 0] = src_data.data[(offset << 2) + 0];
+                    dst_data.data[(offset << 2) + 1] = src_data.data[(offset << 2) + 1];
+                    dst_data.data[(offset << 2) + 2] = src_data.data[(offset << 2) + 2];
+                    dst_data.data[(offset << 2) + 3] = src_data.data[(offset << 2) + 3];
+                } else {
+                    dst_data.data[(offset << 2) + 0] = 0;
+                    dst_data.data[(offset << 2) + 1] = 0;
+                    dst_data.data[(offset << 2) + 2] = 0;
+                    dst_data.data[(offset << 2) + 3] = 0;
+                }
+
+                // next pixel
+                offset++;
+            }
+        }
+
+        // rewrite the display data
+        this.display_.surface.image = dst_data;
+
+        // update the threshold for next frame
+        this.threshold_ += this.increment_;
+        if ((this.threshold_ < 0.0) || (this.threshold_ > 1.0)) {
+            this.increment_ *= -1;
+        }
     }
 
     // render the animation on the screen
