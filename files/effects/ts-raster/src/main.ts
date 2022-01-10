fcfd70f8e5147532a2d3f2b438fadc2ef8e84e73
slegrand <ut5svsxgc@relay.firefox.com>
2022-01-10 18:12:52 -0500

    ts-raster: main entry point for Raster bars effect

---
+++ files/effects/ts-raster/src/main.ts
@@ -0,0 +1,27 @@
+/*
+ * @file    main.ts
+ * @author  Sebastien LEGRAND
+ *
+ * @brief   Main entry point for the Raster bars effect
+ */
+
+//----- imports
+import { StatesManager, States } from "library/core/manager"
+import { Display } from "library/core/display";
+import { RasterBars } from "./raster";
+
+//----- begin
+// retrieve the canvas element from the page
+let display = new Display(<HTMLCanvasElement> document.getElementById("output"));
+
+// create a new instance of the plasma
+let effect = new RasterBars(display);
+
+// create states manager
+let manager = StatesManager.getInstance();
+
+// add a new transition
+manager.add({event: States.S_BEGIN, from: undefined, to: effect});
+
+// start
+manager.start();
