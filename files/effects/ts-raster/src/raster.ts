7a45719ce744ec7cc735350d368c8a74b865df26
slegrand <ut5svsxgc@relay.firefox.com>
2022-01-13 18:25:47 -0500

    ts-raster: load asset image

--- files/effects/ts-raster/src/raster.ts
+++ files/effects/ts-raster/src/raster.ts
@@ -10,16 +10,23 @@ import { IAnimation } from "library/core/animation";
 import { States } from "library/core/manager";
 
 import { Display } from "library/core/display";
+import { Surface } from "library/gfx/surface";
+import { RGBA } from "library/color/RGBA";
 
 
 //----- class
 export class RasterBars extends IAnimation {
 
     //----- members
+    private image_: Surface;    // front image
+
 
     //----- methods
     constructor(display: Display) {
         super('rasterbars', display);
+
+        // initialize members
+        this.image_ = new Surface();
     }
 
     // update the animation
@@ -32,10 +39,26 @@ export class RasterBars extends IAnimation {
     protected render(time?: number): void {
         if (!this.isAnimated)
             return;
+
+        // flip the back-buffer onto the screen
+        this.display_.draw();
     }
 
     // setup function
     public setup(): void {
+        this.image_
+            .loadImage('/images/assets/ts-raster.asset.png')
+            .then(result => {
+                // toggle the animation
+                this.toggle();
+
+                // set the click handler to pause the animation
+                window.onclick = () => {
+                    this.toggle();
+                }
+
+                console.log("Starting the Raster Bars animation");
+            });
     }
 
     // cleanup function
