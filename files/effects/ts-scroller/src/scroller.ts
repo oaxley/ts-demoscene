7a52ad76f58c0f18c83a3c7e4b998a0152274d1b
slegrand <ut5svsxgc@relay.firefox.com>
2022-01-22 16:47:01 -0500

    ts-scroller: load font-map

--- files/effects/ts-scroller/src/scroller.ts
+++ files/effects/ts-scroller/src/scroller.ts
@@ -10,16 +10,35 @@ import { IAnimation } from "library/core/animation";
 import { States } from "library/core/manager";
 
 import { Display } from "library/core/display";
+import { Surface } from "library/gfx/surface";
+import { Font } from "library/gfx/font";
+
+
+//----- globals
+const FONT_CHAR_WIDTH  = 32;
+const FONT_CHAR_HEIGHT = 32;
 
 
 //----- class
 export class Scroller extends IAnimation {
 
     //----- members
+    private fontmap_: Font;
+
 
     //----- methods
     constructor(display: Display) {
         super('text-scroller', display);
+
+        // load the font map
+        this.fontmap_ = new Font(
+                                '/images/assets/font-maop.asset.png',
+                                {
+                                    width: FONT_CHAR_WIDTH,
+                                    height: FONT_CHAR_HEIGHT
+                                }
+        );
+
     }
 
     // update the animation
