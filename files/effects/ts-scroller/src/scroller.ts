88fbf900e8160a77c58954c713d2a3b5a55842f8
slegrand <ut5svsxgc@relay.firefox.com>
2022-01-23 14:27:37 -0500

    ts-scroller: create scroller surfaces

--- files/effects/ts-scroller/src/scroller.ts
+++ files/effects/ts-scroller/src/scroller.ts
@@ -23,12 +23,18 @@ const FONT_CHAR_HEIGHT = 32;
 export class Scroller extends IAnimation {
 
     //----- members
-    private fontmap_: Font;
+    private fontmap_: Font;                 // the font map
+    private fontsfc_: Surface;              // the surface where the text is drawn
+
+    private sprite_: Surface;               // the background sprite
 
 
     //----- methods
     constructor(display: Display) {
         super('text-scroller', display);
+
+        // set the vars
+        this.sprite_ = new Surface();
 
         // load the font map
         this.fontmap_ = new Font(
@@ -39,6 +45,12 @@ export class Scroller extends IAnimation {
                                 }
         );
 
+        // set the font surface wider than the actual display
+        // so we don't have to do expensive calculations for clipping
+        this.fontsfc_ = new Surface({
+            width: display.width + 2 * FONT_CHAR_WIDTH,
+            height: display.height >> 1
+        })
     }
 
     // update the animation
@@ -55,6 +67,19 @@ export class Scroller extends IAnimation {
 
     // setup function
     public setup(): void {
+        this.sprite_
+            .loadImage('/images/assets/ts-scroller.asset.png')
+            .then(result => {
+                // toggle the animation
+                this.toggle();
+
+                // set the click handler to pause the animation
+                window.onclick = () => {
+                    this.toggle();
+                }
+
+                console.log("Starting the Text Scroller animation")
+            });
     }
 
     // cleanup function
