36b9eb2ac7eab1a587a4e98698fd142b9000326b
slegrand <ut5svsxgc@relay.firefox.com>
2022-02-01 18:51:34 -0500

    Webpack-plugin: JSON-merge plugin for config management

---
+++ plugins/json-merge-plugin.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+
+
+class JSONMergePlugin {
+    constructor(options = {}) {
+        this.options = options;
+    }
+
+    apply(compiler) {
+        var config = {
+            "effects": []
+        }
+
+        // read all the JSON configuration files
+        for (var elt in this.options.files) {
+            // retrieve the filename
+            let filename = this.options.files[elt].from;
+
+            // logging
+            console.log(`JSONMergePlugin: Adding JSON config ${filename}`);
+
+            // read the JSON data
+            let content = fs.readFileSync(filename, 'utf8');
+            let data = JSON.parse(content);
+
+            // add the data to the global configuration item
+            config['effects'].push(data);
+        };
+
+        // create the output directory
+        const dir  = path.dirname(this.options.output);
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        // save the output file
+        let filename = path.relative(".", this.options.output);
+        console.log(`JSONMergePlugin: Writing JSON ${filename}`);
+        fs.writeFileSync(filename, JSON.stringify(config, null, 4), 'utf-8');
+    }
+}
+
+module.exports = JSONMergePlugin;
